a2aae5aff3a7598e50925794ff4aeb24fdc70e33
zvodd <zv.odd.101@gmail.com>
2021-11-24 21:39:03 +0800

    impliment multi search

--- Rightclick-to-YouTube/background.js
+++ Rightclick-to-YouTube/background.js
@@ -1,19 +1,50 @@
-function searchClickHandler(info, tab) {
-    var newURL = "http://www.youtube.com/results?search_query=" + info.selectionText;
-    chrome.tabs.create({
-        url: newURL,
-        selected: true,
-        index: tab.index + 1
-    });
+
+
+var searchProviders = [
+    {
+        name:"Youtube",
+        furl: "http://www.youtube.com/results?search_query={search}"
+    },
+    {
+        name:"ebay",
+        furl:"http://www.ebay.com/sch/?_nkw={search}"
+    }
+]
+
+function menuHandlerFunc(provider){
+
+    function format(str, args) {
+        for (const arg in args) str = str.replace(new RegExp(`\\{${arg}\\}`, 'gi'), args[arg])
+      return str
+    }
+
+    return function searchClickHandler(info, tab) {
+        var newURL = format(provider.furl, {search: info.selectionText});
+        chrome.tabs.create({
+            url: newURL,
+            selected: true,
+            index: tab.index + 1
+        });
+    }
+    
 }
 
 
 chrome.contextMenus.removeAll(
     function() {
-        var id = chrome.contextMenus.create({
-            title: "Search YouTube for “%s”",
+        let root = chrome.contextMenus.create({
+            title: "Search “%s”",
             contexts: ["selection"],
-            onclick: searchClickHandler
+            // onclick: searchClickHandler
         });
+        for (let sp of searchProviders){
+            chrome.contextMenus.create({
+                parentId: root,
+                title: sp.name, // todo escape % ??
+                contexts: ["selection"],
+                onclick: menuHandlerFunc(sp)
+            })
+        }
     }
 );
+
